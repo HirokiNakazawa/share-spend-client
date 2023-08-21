62ac6aadb239e780c4718fed7565b7c060f29670
nakazawa <sp.hn_hc4@i.softbank.jp>
2023-08-21 17:33:01 +0900

    ダッシュボードのレイアウト作成

--- src/app/components/DashBoard.tsx
+++ src/app/components/DashBoard.tsx
@@ -1,13 +1,34 @@
-import { Container, Typography } from "@mui/material";
+import { Container, Grid, Paper, Typography } from "@mui/material";
 import { FC } from "react";
-import CustomModal from "./CunstomModal";
 
 const DashBoard: FC = () => {
   return (
-    <Container>
-      <CustomModal />
-      <Typography>Hello Unko</Typography>
-    </Container>
+    <>
+      <Container style={{ maxWidth: "90%" }}>
+        <Grid container spacing={1} mt={1}>
+          <Grid item xs={8} height="60vh">
+            <Paper elevation={3} sx={{ height: "100%" }}>
+              <Typography>hoge</Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper elevation={3} sx={{ height: "100%" }}>
+              <Typography>hoge</Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={4} height="30vh">
+            <Paper elevation={3} sx={{ height: "100%" }}>
+              <Typography>hoge</Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={8}>
+            <Paper elevation={3} sx={{ height: "100%" }}>
+              <Typography>hoge</Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import { Box, Container, Typography } from "@mui/material";
 import { FC } from "react";
+import CustomModal from "./components/CunstomModal";
 import Header from "./components/Header";
 import DashBoard from "./components/DashBoard";
 
 const Home: FC = () => {
   return (
     <>
+      <CustomModal />
       <Header />
       <DashBoard />
     </>
