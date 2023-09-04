20bf4c5c0696eb3ff1428000a168de70737e3667
nakazawa <sp.hn_hc4@i.softbank.jp>
2023-09-04 16:17:01 +0900

    円グラフの更新をサーバーサイドレンダリングしないように修正

--- src/features/Dashboard/LeftTopAria/components/MainLeftAria.tsx
+++ src/features/Dashboard/LeftTopAria/components/MainLeftAria.tsx
@@ -1,10 +1,15 @@
 import { Box } from "@mui/material";
 import { FC } from "react";
 import AriaName from "@/features/Dashboard/LeftTopAria/components/AriaName";
-import PieChartContainer from "@/features/Dashboard/LeftTopAria/containers/PieChartContainer";
 import AriaClaim from "@/features/Dashboard/LeftTopAria/components/AriaClaim";
+import dynamic from "next/dynamic";
 
 const MainLeftAria: FC = () => {
+  const PieChartContainer = dynamic(
+    () =>
+      import("@/features/Dashboard/LeftTopAria/containers/PieChartContainer"),
+    { ssr: false }
+  );
   return (
     <Box sx={{ width: "70%", position: "relative" }}>
       <AriaName />
--- src/features/Dashboard/LeftTopAria/containers/PieChartContainer.tsx
+++ src/features/Dashboard/LeftTopAria/containers/PieChartContainer.tsx
@@ -3,13 +3,10 @@
 import { FC } from "react";
 import { useRecoilValue } from "recoil";
 import { monthlyCostByTypeState } from "@/recoil/monthlyState";
-import dynamic from "next/dynamic";
+import MainPieChart from "@/features/Dashboard/LeftTopAria/components/MainPieChart";
 
 const PieChartContainer: FC = () => {
   const monthlyCostByType = useRecoilValue(monthlyCostByTypeState);
-  const MainPieChart = dynamic(() => import("../components/MainPieChart"), {
-    ssr: false,
-  });
 
   return <MainPieChart data={monthlyCostByType} />;
 };
