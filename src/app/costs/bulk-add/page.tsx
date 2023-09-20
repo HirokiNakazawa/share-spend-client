3a63ab82a793cd6f7190bbf364e34e2484d662b8
nakazawa <sp.hn_hc4@i.softbank.jp>
2023-09-20 10:42:39 +0900

    固定費関連のページ作成

--- src/app/costs/bulk-add/page.tsx
+++ src/app/costs/bulk-add/page.tsx
@@ -1,8 +1,10 @@
+import { FC } from "react";
+
 import ModalContainer from "@/features/Modal/containers/ModalContainer";
 import HeaderContainer from "@/features/Header/containers/HeaderContainer";
 import CostManagementLayoutContainer from "@/features/CostManagement/containers/CostManagementLayoutContainer";
 
-const Home = () => {
+const Home: FC = () => {
   return (
     <>
       <ModalContainer />
---
+++ src/app/fixed-cost/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+import HeaderContainer from "@/features/Header/containers/HeaderContainer";
+
+const Home: FC = () => {
+  return (
+    <>
+      <HeaderContainer />
+    </>
+  );
+};
+
+export default Home;
